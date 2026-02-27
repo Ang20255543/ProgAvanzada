e5d8bd00a6434b420c7dbdc9f4804f848e7a3a1c
Ang20255543 <aeduardo.hdz20@gmail.com>
2026-02-27 17:59:29 -0600

    ss

--- Fase 2/Transportes/clases.py
+++ Fase 2/Transportes/clases.py
@@ -4,11 +4,10 @@ class Transporte:
     modelo = ""
     km = 0
 
-    def __init__(self, codigo, marca, modelo, km):
+    def __init__(self, codigo, marca, modelo):
         self.codigo = codigo
         self.marca = marca
         self.modelo = modelo
-        self.km = km
 
     def __str__(self):
         salida = f"Codigo: {self.codigo}\nMarca: {self.marca}\nModelo: {self.modelo}\nKm: {self.km}"
