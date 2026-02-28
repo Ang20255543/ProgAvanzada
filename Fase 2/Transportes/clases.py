f8896081fe462d114812ddf8f92e55e82b8d50d6
Ang20255543 <aeduardo.hdz20@gmail.com>
2026-02-27 18:56:03 -0600

    eso eso

--- Fase 2/Transportes/clases.py
+++ Fase 2/Transportes/clases.py
@@ -4,10 +4,11 @@ class Transporte:
     modelo = ""
     km = 0
 
-    def __init__(self, codigo, marca, modelo):
+    def __init__(self, codigo, marca, modelo, km=0):
         self.codigo = codigo
         self.marca = marca
         self.modelo = modelo
+        self.km = km
 
     def __str__(self):
         salida = f"Codigo: {self.codigo}\nMarca: {self.marca}\nModelo: {self.modelo}\nKm: {self.km}"
--- Fase 2/Transportes/main.py
+++ Fase 2/Transportes/main.py
@@ -6,6 +6,7 @@ marca = input("Marca: ").upper()
 modelo = get_int("Modelo: ")
 
 camion = Transporte(codigo, marca, modelo, 0)
+camion = Transporte("AZ-12", "Ford", 2025, 100)
 
 print(f"Codigo: {camion.codigo}")
 print(f"Marca: {camion.marca}")
