109a56726afa285c0242fee372708256471b19fe
Ang20255543 <aeduardo.hdz20@gmail.com>
2026-03-20 19:10:10 -0600

    cambio ejercicoo 1

--- Fase 2/ejercicio_1.py
+++ Fase 2/ejercicio_1.py
@@ -8,7 +8,6 @@ fecha_str = input ("Ingrsa una fecha en formato (aaaa-mm-dd): ")
 try:
     fecha = dt.strptime(fecha_str, "%Y-%m-%d")
     fecha_lg = fecha.strftime("%A, %d de %B de %Y")
-    print (f"La fecha ingresada es: {fecha_lg}")
+    print (f"La fecha ingresada es: {fecha_lg.capitalize()}")
 except ValueError:
     print ("La fecha ingresada no es válida. Por favor, ingresa una fecha en el formato correcto (aaaa-mm-dd).")
-    
