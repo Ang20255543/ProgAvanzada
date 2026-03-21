a23185c5b22985951930454dfe2aca1cf5bfccef
Ang20255543 <aeduardo.hdz20@gmail.com>
2026-03-20 19:09:19 -0600

    ejercico 1

---
+++ Fase 2/ejercicio_1.py
@@ -0,0 +1,14 @@
+from datetime import datetime as dt 
+import locale 
+
+locale.setlocale(locale.LC_ALL, "es_MX")
+
+fecha_str = input ("Ingrsa una fecha en formato (aaaa-mm-dd): ")
+
+try:
+    fecha = dt.strptime(fecha_str, "%Y-%m-%d")
+    fecha_lg = fecha.strftime("%A, %d de %B de %Y")
+    print (f"La fecha ingresada es: {fecha_lg}")
+except ValueError:
+    print ("La fecha ingresada no es válida. Por favor, ingresa una fecha en el formato correcto (aaaa-mm-dd).")
+    
