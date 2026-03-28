8abc8009d12a3435bac38334b00e052495ca81fc
Ang20255543 <aeduardo.hdz20@gmail.com>
2026-03-27 18:36:34 -0600

    ejercicion 4: Crear un programa que permita al usuario ingresar el nombre de una materia y su calificación, almacenando esta información en un diccionario. El programa debe permitir ingresar varias materias y calificaciones, y al finalizar, mostrar todas las materias con sus respectivas calificaciones, así como el promedio de las calificaciones ingresadas.

---
+++ Fase 2/ejercicios/ejercicio_4.py
@@ -0,0 +1,17 @@
+# CALIFICACIONES
+materias = dict()
+
+while True:
+    materia = input("Ingrese el nombre de la materia: ")
+
+    if materia == "x":
+        break
+    calif = int(input("Ingrese la calificacion: "))
+
+    materias [materia] = calif
+
+for mat, cali in zip(materias.keys(), materias.values()):
+    print (f"La materia ingresada fue {mat} y la calificacion es {cali}")
+
+promedio = sum(materias.values()) / len(materias)
+print(f"El promedio de las calificaciones es: {promedio}")
