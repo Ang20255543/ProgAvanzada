f50f07b2d7fdb11fe2081d141b9b52836f8f3718
Ang20255543 <aeduardo.hdz20@gmail.com>
2026-04-14 19:44:32 -0600

    eso eso

---
+++ Fase 2/CRUD/clases.py
@@ -0,0 +1,17 @@
+class Producto:
+    clave = ""
+    nombre = ""
+    marca = ""
+    precio = ""
+    existencia = 0
+
+    def __init__(self, clave: int, nombre: str, marca: str, precio: float, existencia: int = 0):
+        self.clave = clave
+        self.nombre = nombre
+        self.marca = marca
+        self.precio = precio
+        self.existencia = existencia
+
+    def __str__(self):
+        salida =  f"Clave: {self.clave}, Nombre: {self.nombre}, Marca: {self.marca}Precio: {self.precio}, Existencia: {self.existencia}" 
+        return salida
---
+++ Fase 2/CRUD/main.py
@@ -0,0 +1,4 @@
+from clases import Producto
+producto = Producto(123, "Laptop", "Dell", 31, 10)
+
+print (producto)
